e2318c75aa89c2bf89412a6cceb82739c9071047
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-06-24 19:19:20 +0530

    done with the logic

---
+++ app.js
@@ -0,0 +1,19 @@
+const message_btn = document.querySelector(".btn");
+
+message_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const message = document.querySelector(".box").value;
+
+  if (message == "") {
+    document.querySelector(".feedback").style.display = "flex";
+    const message_placeholder = (document.querySelector(
+      ".message-content"
+    ).innerHTML = "");
+  } else {
+    document.querySelector(".feedback").style.display = "none";
+    const message_placeholder = (document.querySelector(
+      ".message-content"
+    ).innerHTML = message);
+  }
+});
